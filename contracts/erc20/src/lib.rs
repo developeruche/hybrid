6c55f5f6b72b2898b5b15c2f86f04eabfc3ad3d0
developeruche <developeruche@gmail.com>
2025-05-13 20:09:01 +0100

    fix erc20 contract

--- contracts/erc20/src/lib.rs
+++ contracts/erc20/src/lib.rs
@@ -2,63 +2,193 @@
 #![no_main]
 
 use core::default::Default;
-
 use contract_derive::{contract, payable, storage, Event, Error};
 use eth_riscv_runtime::types::*;
-
 use alloy_core::primitives::{Address, U256};
-
 extern crate alloc;
 
 // -- EVENTS -------------------------------------------------------------------
 #[derive(Event)]
-pub struct StarageSet {
-    pub storage_item: U256,
+pub struct Transfer {
+    #[indexed]
+    pub from: Address,
+    #[indexed]
+    pub to: Address,
+    pub amount: U256,
 }
 
+#[derive(Event)]
+pub struct Approval {
+    #[indexed]
+    pub owner: Address,
+    #[indexed]
+    pub spender: Address,
+    pub amount: U256,
+}
 
+#[derive(Event)]
+pub struct OwnershipTransferred {
+    #[indexed]
+    pub from: Address,
+    #[indexed]
+    pub to: Address,
+}
 
 // -- ERRORS -------------------------------------------------------------------
 #[derive(Error)]
-pub enum StorageError {
-    FailedToSetStorage
+pub enum ERC20Error {
+    OnlyOwner,
+    InsufficientBalance(U256),
+    InsufficientAllowance(U256),
+    SelfApproval,
+    SelfTransfer,
+    ZeroAmount,
+    ZeroAddress,
 }
 
 // -- CONTRACT -----------------------------------------------------------------
 #[storage]
-pub struct Storage {
-    storage_item: Slot<U256>,
+pub struct ERC20 {
+    total_supply: Slot<U256>,
+    balance_of: Mapping<Address, Slot<U256>>,
+    allowance_of: Mapping<Address, Mapping<Address, Slot<U256>>>,
+    owner: Slot<Address>,
+    // TODO: handle string storage
+    // name: String, 
+    // symbol: String,
+    // decimals: u8,
 }
 
 #[contract]
-impl Storage {
+impl ERC20 {
     // -- CONSTRUCTOR ----------------------------------------------------------
-    pub fn new(init_item: U256) -> Self {
+    pub fn new(owner: Address) -> Self {
         // Init the contract
-        let mut storage = Storage::default();
+        let mut erc20 = ERC20::default();
 
         // Update state
-        storage.storage_item.write(init_item);
+        erc20.owner.write(owner);
 
         // Return the initialized contract
-        storage
+        erc20
     }
 
     // -- STATE MODIFYING FUNCTIONS --------------------------------------------
-    pub fn set_storage(&mut self, item: U256) -> Result<bool, StorageError> {
-        let from = msg_sender();
+    #[payable]
+    pub fn mint(&mut self, to: Address, amount: U256) -> Result<bool, ERC20Error> {
+        // Perform sanity checks
+        if msg_sender() != self.owner.read() { return Err(ERC20Error::OnlyOwner) }; 
+        if amount == U256::ZERO { return Err(ERC20Error::ZeroAmount) };
+        if to == Address::ZERO { return Err(ERC20Error::ZeroAddress) };
 
-        // Update state
-        self.storage_item.write(item);
+        // Increase user balance
+        let to_balance = self.balance_of[to].read();
+        self.balance_of[to].write(to_balance + amount);
 
-        // Emit event + return 
-        log::emit(StarageSet::new(item));
+        // Increase total supply
+        self.total_supply += amount;
+        
+        // Emit event + return `true` to stick to (EVM) ERC20 convention
+        log::emit(Transfer::new(Address::ZERO, to, amount));
         Ok(true)
     }
 
+    pub fn approve(&mut self, spender: Address, amount: U256) -> Result<bool, ERC20Error> {
+        let owner = msg_sender();
+
+        // Perform sanity checks
+        if spender == Address::ZERO { return Err(ERC20Error::ZeroAddress) };
+        if spender == owner { return Err(ERC20Error::SelfApproval) };
+
+        // Update state
+        self.allowance_of[owner][spender].write(amount);
+
+        // Emit event + return 
+        log::emit(Approval::new(owner, spender, amount));
+        Ok(true)
+    }
+
+    pub fn transfer(&mut self, to: Address, amount: U256) -> Result<bool, ERC20Error> {
+        let from = msg_sender();
+
+        // Perform sanity checks
+        if to == Address::ZERO { return Err(ERC20Error::ZeroAddress) };
+        if amount == U256::ZERO { return Err(ERC20Error::ZeroAmount) };
+        if from == to { return Err(ERC20Error::SelfTransfer) };
+
+        // Read user balances
+        let from_balance = self.balance_of[from].read();
+        let to_balance = self.balance_of[to].read();
+
+        // Ensure enough balance
+        if from_balance < amount { return Err(ERC20Error::InsufficientBalance(from_balance)) }
+
+        // Update state
+        self.balance_of[from].write(from_balance - amount);
+        self.balance_of[to].write(to_balance + amount);
+
+        // Emit event + return 
+        log::emit(Transfer::new(from, to, amount));
+        Ok(true)
+    }
+
+    pub fn transfer_from(&mut self, from: Address, to: Address, amount: U256) -> Result<bool, ERC20Error> {
+        let msg_sender = msg_sender();
+
+        // Perform sanity checks
+        if to == Address::ZERO { return Err(ERC20Error::ZeroAddress) };
+        if amount == U256::ZERO { return Err(ERC20Error::ZeroAmount) };
+        if from == to { return Err(ERC20Error::SelfTransfer) };
+
+        // Ensure enough allowance
+        let allowance = self.allowance_of[from][msg_sender].read();
+        if allowance < amount { return Err(ERC20Error::InsufficientAllowance(allowance)) };
+
+        // Ensure enough balance
+        let from_balance = self.balance_of[from].read();
+        if from_balance < amount { return Err(ERC20Error::InsufficientBalance(from_balance)) };
+
+        // Update state
+        self.allowance_of[from][msg_sender].write(allowance - amount);
+        self.balance_of[from].write(from_balance - amount);
+        
+        let to_balance = self.balance_of[to].read();
+        self.balance_of[to].write(to_balance + amount);
+
+        // Emit event + return 
+        log::emit(Transfer::new(from, to, amount));
+        Ok(true)
+    }
+
+    pub fn transfer_ownership(&mut self, new_owner: Address) -> Result<bool, ERC20Error> {
+        let from = msg_sender();
+
+        // Perform safety check 
+        if from != self.owner.read() { return Err(ERC20Error::OnlyOwner) }; 
+        if from == new_owner { return Err(ERC20Error::SelfTransfer) }; 
+
+        // Update state
+        self.owner.write(new_owner);
+
+        // Emit event + return 
+        log::emit(OwnershipTransferred::new(from, new_owner));
+        Ok(true)
+    }
 
     // -- READ-ONLY FUNCTIONS --------------------------------------------------
-    pub fn read_item(&self) -> U256 {
-        self.storage_item.read()
+    pub fn owner(&self) -> Address {
+        self.owner.read()
+    }
+
+    pub fn total_supply(&self) -> U256 {
+        self.total_supply.read()
+    }
+
+    pub fn balance_of(&self, owner: Address) -> U256 {
+        self.balance_of[owner].read()
+    }
+
+    pub fn allowance(&self, owner: Address, spender: Address) -> U256 {
+        self.allowance_of[owner][spender].read()
     }
 }
