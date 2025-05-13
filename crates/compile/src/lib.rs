0d68f711b301c976ad14fba0419acc36e7fc4178
developeruche <developeruche@gmail.com>
2025-05-13 22:21:45 +0100

    do with deployable

--- crates/compile/src/lib.rs
+++ crates/compile/src/lib.rs
@@ -1,3 +1,15 @@
 //! Compiling RUST smart contract targetting RISCV
+use std::{fs, path::Path};
+
+use utils::obtain_contract_by_path;
 pub mod primitivies;
 pub mod utils;
+
+pub fn run_contract_compilation(contract_root: &Path) -> Result<(), anyhow::Error> {
+    let output_dir = contract_root.join("out");
+    fs::create_dir_all(&output_dir)?;
+
+    let contract = obtain_contract_by_path(contract_root);
+
+    Ok(())
+}
