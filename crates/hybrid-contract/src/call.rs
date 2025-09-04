398a11aa4c4ddad44360938296296de81aeef4cb
developeruche <developeruche@gmail.com>
2025-09-04 22:31:17 +0100

    ported hybrid contract

---
+++ crates/hybrid-contract/src/call.rs
@@ -0,0 +1,166 @@
+extern crate alloc;
+use alloc::vec::Vec;
+use alloy_core::primitives::{Address, Bytes, U256};
+use core::{arch::asm, marker::PhantomData};
+use hybrid_syscalls::Syscall;
+
+// Concrete types implementing the context traits
+pub struct ReadOnly;
+pub struct ReadWrite;
+
+// Marker traits to determine call context
+pub trait CallCtx {}
+pub trait StaticCtx: CallCtx {}
+pub trait MutableCtx: StaticCtx {}
+
+impl CallCtx for ReadOnly {}
+impl CallCtx for ReadWrite {}
+impl StaticCtx for ReadOnly {}
+impl StaticCtx for ReadWrite {}
+impl MutableCtx for ReadWrite {}
+
+// Marker trait to connect contract method context with call ctx
+pub trait MethodCtx { type Allowed: CallCtx; }
+impl<'a, T> MethodCtx for &'a T { type Allowed = ReadOnly; }
+impl<'a, T> MethodCtx for &'a mut T { type Allowed = ReadWrite; }
+
+// Types and traits to build a MethodCtx-aware interface
+pub struct InterfaceBuilder<I> {
+    pub address: Address,
+    pub _phantom: PhantomData<I>,
+}
+
+pub trait InitInterface: Sized {
+    fn new(address: Address) -> InterfaceBuilder<Self>;
+}
+
+// Key change: Add trait to convert between interface types
+pub trait IntoInterface<T> {
+    fn into_interface(self) -> T;
+}
+
+impl<I> InterfaceBuilder<I> {
+    pub fn with_ctx<M: MethodCtx, T>(
+        self,
+        _: M
+    ) -> T 
+    where
+        I: IntoInterface<T>,
+        M: MethodCtx<Allowed = T::Context>,
+        T: FromBuilder
+    {
+        let target_builder = InterfaceBuilder {
+            address: self.address,
+            _phantom: PhantomData,
+        };
+        T::from_builder(target_builder)
+    }
+}
+
+pub trait FromBuilder: Sized {
+    type Context: CallCtx;
+    fn from_builder(builder: InterfaceBuilder<Self>) -> Self;
+}
+
+
+/// Trait for contracts to have an entry point for txs  
+pub trait Contract {
+    fn call(&mut self);
+    fn call_with_data(&mut self, calldata: &[u8]);
+}
+
+pub fn call_contract(
+    addr: Address,
+    value: u64,
+    data: &[u8],
+    ret_size: Option<u64>,
+) -> Bytes {
+    // Perform the call without writing return data into (REVM) memory
+    call(addr, value, data.as_ptr() as u64, data.len() as u64);
+    // Load call output to memory
+    handle_call_output(ret_size)
+}
+
+pub fn call(addr: Address, value: u64, data_offset: u64, data_size: u64) {
+    let addr: U256 = addr.into_word().into();
+    let addr = addr.as_limbs();
+    unsafe {
+        asm!(
+            "ecall",
+            in("a0") addr[0], in("a1") addr[1], in("a2") addr[2],
+            in("a3") value, in("a4") data_offset, in("a5") data_size,
+            in("t0") u8::from(Syscall::Call)
+        );
+    }
+}
+
+pub fn staticcall_contract(addr: Address, value: u64, data: &[u8], ret_size: Option<u64>) -> Bytes {
+    // Perform the staticcall without writing return data into (REVM) memory
+    staticcall(addr, value, data.as_ptr() as u64, data.len() as u64);
+    // Load call output to memory
+    handle_call_output(ret_size)
+}
+
+fn handle_call_output(ret_size: Option<u64>) -> Bytes {
+    // Figure out return data size + initialize memory location
+    let ret_size = match ret_size {
+        Some(size) => size,
+        None => return_data_size(),
+    };
+  
+    if ret_size == 0 {
+        return Bytes::default()
+    };
+
+    let mut ret_data = Vec::with_capacity(ret_size as usize);
+    ret_data.resize(ret_size as usize, 0);
+
+    // Copy the return data from the interpreter's buffer
+    let (offset, chunks, remainder) = (ret_data.as_ptr() as u64, ret_size / 32, ret_size % 32);
+
+    // handle full chunks
+    for i in 0..chunks {
+        let step = i * 32;
+        return_data_copy(offset + step, step, 32);
+    };
+
+    // handle potential last partial-chunk
+    if remainder != 0 {
+        let step = chunks * 32;
+        return_data_copy(offset + step, step, remainder);
+    };
+
+    Bytes::from(ret_data)
+}
+
+pub fn staticcall(addr: Address, value: u64, data_offset: u64, data_size: u64) {
+    let addr: U256 = addr.into_word().into();
+    let addr = addr.as_limbs();
+    unsafe {
+        asm!(
+            "ecall",
+            in("a0") addr[0], in("a1") addr[1], in("a2") addr[2],
+            in("a3") value, in("a4") data_offset, in("a5") data_size,
+            in("t0") u8::from(Syscall::StaticCall)
+        );
+    }
+}
+
+pub fn return_data_size() -> u64 {
+    let size: u64;
+    unsafe {
+        asm!( "ecall", lateout("a0") size, in("t0") u8::from(Syscall::ReturnDataSize));
+    }
+
+    size
+}
+
+pub fn return_data_copy(dest_offset: u64, res_offset: u64, res_size: u64) {
+    unsafe {
+        asm!(
+            "ecall",
+            in("a0") dest_offset, in("a1") res_offset, in("a2") res_size, in("t0")
+            u8::from(Syscall::ReturnDataCopy)
+        );
+    }
+}
--- crates/hybrid-contract/src/lib.rs
+++ crates/hybrid-contract/src/lib.rs
@@ -1,1 +1,141 @@
-pub mod allocator;
+#![no_std]
+#![no_main]
+#![feature(alloc_error_handler, maybe_uninit_write_slice, round_char_boundary)]
+
+use alloy_core::primitives::{Address, U256};
+use core::{arch::asm, fmt::Write, panic::PanicInfo, slice};
+extern crate alloc as ext_alloc;
+
+mod allocator;
+pub mod env;
+pub mod tx;
+pub mod hstd;
+
+pub mod create;
+pub use create::Deployable;
+
+pub mod error;
+pub use error::{revert, revert_with_error, Error};
+
+pub mod log;
+pub use log::{emit_log, Event};
+
+pub mod call;
+pub use call::*;
+
+const CALLDATA_ADDRESS: usize = 0x8000_0000;
+
+pub unsafe fn slice_from_raw_parts(address: usize, length: usize) -> &'static [u8] {
+    slice::from_raw_parts(address as *const u8, length)
+}
+
+#[panic_handler]
+unsafe fn panic(info: &PanicInfo<'_>) -> ! {
+    static mut IS_PANICKING: bool = false;
+
+    if !IS_PANICKING {
+        IS_PANICKING = true;
+
+        // Capture the panic info msg
+        let mut message = ext_alloc::string::String::new();
+        let _ = write!(message, "{:?}", info.message());
+
+        // Convert to bytes and revert
+        let msg = message.into_bytes();
+        revert_with_error(&msg);
+    } else {
+        revert_with_error("Panic handler has panicked!".as_bytes())
+    }
+}
+
+use eth_riscv_syscalls::Syscall;
+
+pub fn return_riscv(addr: u64, offset: u64) -> ! {
+    unsafe {
+        asm!("ecall", in("a0") addr, in("a1") offset, in("t0") u8::from(Syscall::Return));
+    }
+    unreachable!()
+}
+
+pub fn sload(key: U256) -> U256 {
+    let key = key.as_limbs();
+    let (val0, val1, val2, val3): (u64, u64, u64, u64);
+    unsafe {
+        asm!(
+            "ecall",
+            lateout("a0") val0, lateout("a1") val1, lateout("a2") val2, lateout("a3") val3,
+            in("a0") key[0], in("a1") key[1], in("a2") key[2], in("a3") key[3],
+            in("t0") u8::from(Syscall::SLoad));
+    }
+    U256::from_limbs([val0, val1, val2, val3])
+}
+
+pub fn sstore(key: U256, value: U256) {
+    let key = key.as_limbs();
+    let value = value.as_limbs();
+
+    unsafe {
+        asm!(
+            "ecall",
+            in("a0") key[0], in("a1") key[1], in("a2") key[2], in("a3") key[3],
+            in("a4") value[0], in("a5") value[1], in("a6") value[2], in("a7") value[3],
+            in("t0") u8::from(Syscall::SStore)
+        );
+    }
+}
+
+pub fn keccak256(offset: u64, size: u64) -> U256 {
+    let (first, second, third, fourth): (u64, u64, u64, u64);
+    unsafe {
+        asm!(
+            "ecall",
+            in("a0") offset,
+            in("a1") size,
+            lateout("a0") first,
+            lateout("a1") second,
+            lateout("a2") third,
+            lateout("a3") fourth,
+            in("t0") u8::from(Syscall::Keccak256)
+        );
+    }
+    U256::from_limbs([first, second, third, fourth])
+}
+
+pub fn msg_sender() -> Address {
+    let (first, second, third): (u64, u64, u64);
+    unsafe {
+        asm!("ecall", lateout("a0") first, lateout("a1") second, lateout("a2") third, in("t0") u8::from(Syscall::Caller));
+    }
+    let mut bytes = [0u8; 20];
+    bytes[0..8].copy_from_slice(&first.to_be_bytes());
+    bytes[8..16].copy_from_slice(&second.to_be_bytes());
+    bytes[16..20].copy_from_slice(&third.to_be_bytes()[..4]);
+    Address::from_slice(&bytes)
+}
+
+pub fn msg_value() -> U256 {
+    let (first, second, third, fourth): (u64, u64, u64, u64);
+    unsafe {
+        asm!("ecall", lateout("a0") first, lateout("a1") second, lateout("a2") third, lateout("a3") fourth, in("t0") u8::from(Syscall::CallValue));
+    }
+    U256::from_limbs([first, second, third, fourth])
+}
+
+pub fn msg_sig() -> [u8; 4] {
+    let sig = unsafe { slice_from_raw_parts(CALLDATA_ADDRESS + 8, 4) };
+    sig.try_into().unwrap()
+}
+
+pub fn msg_data() -> &'static [u8] {
+    let length = unsafe { slice_from_raw_parts(CALLDATA_ADDRESS, 8) };
+    let length = u64::from_le_bytes([
+        length[0], length[1], length[2], length[3], length[4], length[5], length[6], length[7],
+    ]) as usize;
+    unsafe { slice_from_raw_parts(CALLDATA_ADDRESS + 8, length) }
+}
+
+#[allow(non_snake_case)]
+#[no_mangle]
+fn DefaultHandler() {
+    panic!("default handler");
+}
